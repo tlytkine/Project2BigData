6b50713480f35d2848617100c88c2713891ca01c
Timothy Lytkine <tlytkine@gmail.com>
2019-03-21 14:18:57 -0400

    changes

---
+++ Lecture5.R
@@ -0,0 +1,11 @@
+# Lecture 5 Predictive Analytics 
+# Predictive Models: identify possible outcomes by analyzing data
+
+# Decision Models: what are the possible decisions that will yield a net benefit?
+# -Need to have good domain knowledge 
+
+# Association Models: based on underlying relationships and associations in the data
+
+BlackFriday <- read_excel("BlackFriday.xlsx")
+black_friday
+
--- Project2Tim.R
+++ Project2Tim.R
@@ -7,9 +7,11 @@ library(readxl)
 library(dplyr)
 library(psych)
 library(ggplot2)
-BlackFriday <- read_excel("BlackFriday.xlsx")
+library(base)
+BlackFriday <- read_excel("BlackFriday.xlsx",col_names=TRUE)
 # original data set 
 BlackFriday
+
 # Summary
 summary(BlackFriday)
 # Describe 
@@ -20,9 +22,58 @@ BlackFridayClean <- BlackFriday
 BlackFridayClean <- na.omit(BlackFridayClean)
 # View BlackFridayClean
 BlackFridayClean
+colnames(BlackFridayClean)
+bf <- BlackFridayClean
+colnames(bf)
+
+bf$City_Category[bf$City_Category=="A"] <- "DC"
+bf$City_Category[bf$City_Category=="B"] <- "NYC"
+bf$City_Category[bf$City_Category=="C"] <- "Chicago"
+bf
+
+cities <- split(bf,bf$City_Category)
+
+cities
+NYC <- cities$NYC
+DC <- cities$DC
+Chicago <- cities$Chicago
+
+NYC
+DC
+Chicago
+
+summary(NYC)
+summary(DC)
+summary(Chicago)
 
 
+describe(NYC)
+describe(DC)
+describe(Chicago)
 
+str(NYC)
+psych::describe(NYC,na.rm=TRUE)
+NYC.clean <- na.omit(NYC)
+NYC.clean
+NYC.clean$Marital_Status
+
+NYC.data <- select(NYC,User_ID,Gender,Age,Occupation,Purchase,Marital_Status)
+NYC.data$Gender
+
+NYC.data.gender <- split(NYC.data,NYC.data$Gender)
+
+NYC.data.male <- NYC.data.gender$M
+NYC.data.female <- NYC.data.gender$F
+
+NYC.data.male.maritalstatus <- split(NYC.data.male,NYC.data.male$Marital_Status)
+NYC.data.female.maritalstatus <- split(NYC.data.female,NYC.data.female$Marital_Status)
+
+NYC.data.male.single <- NYC.data.male.maritalstatus$`0`
+NYC.data.male.married <- NYC.data.male.maritalstatus$`1`
+NYC.data.male.married
+
+NYC.data.female.single <- NYC.data.female.maritalstatus$`0`
+NYC.data.female.married <- NYC.data.female.maritalstatus$`1`
 
 
 
